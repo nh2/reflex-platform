bfcac060cf5f41d25c948a71029d475d5ea92def
Niklas Hambüchen <mail@nh2.me>
2017-02-21 12:46:45 +0100

    Tutorial step 3: Reacting to button with Javascript native functions

--- haskellerz-reflex-tutorial.hs
+++ haskellerz-reflex-tutorial.hs
@@ -14,10 +14,17 @@
 --   reflex-dom: https://github.com/reflex-frp/reflex-dom/blob/develop/Quickref.md
 
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wall #-}
 
+-- Library imports
+import           Control.Monad.IO.Class
 import           Data.Monoid
-
+-- GHCJS specific imports
+import           GHCJS.Types (JSString)
+import           GHCJS.DOM.Types (toJSString)
+-- Reflex specific imports
+import           Reflex
 import           Reflex.Dom
 
 
@@ -25,8 +32,26 @@ tutorialSection :: (MonadWidget t m) => m () -> m ()
 tutorialSection widgets = divClass "tutorial-section" widgets
 
 
+-- | Convenient function that pops up a javascript alert dialog box when an
+-- event fires with a message to display.
+--
+-- Taken from the `reflex-dom-contrib` package.
+alertEvent :: forall t m a . MonadWidget t m => (a -> String) -> Event t a -> m ()
+alertEvent eventValueToStr e = performEvent_ (alert <$> e)
+  where
+    -- alert :: (MonadIO m) => String -> IO ()
+    alert a = liftIO $ js_alert $ toJSString $ eventValueToStr a
+
+foreign import javascript unsafe
+  "alert($1)"
+  js_alert :: JSString -> IO ()
+
+
 main :: IO ()
-main = mainWidget $ do
+main = mainWidget myWidgets
+
+myWidgets :: (MonadWidget t m) => m ()
+myWidgets = do
   elAttr
     "link"
     ("href" =: "../haskellerz-reflex-tutorial.css" <>
@@ -47,3 +72,13 @@ main = mainWidget $ do
     el "p" $ do
       el "span" $ do
         text "Text in a span in a paragraph."
+
+  tutorialSection $ do
+    -- The type returned by a button is an Event that contains
+    -- () -- no more information than the fact that the event occurred.
+    -- buttonEv :: Event t () <- button "Click me"
+    -- But of course, we may leave the type away, as Haskell infers it.
+    buttonEv <- button "Click me"
+    alertEvent
+      (\val -> "The button was clicked; the event contained: " ++ show val)
+      buttonEv
